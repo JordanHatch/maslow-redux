cfb379240f4bb0cca5ef79affc7097615575dcfe
Jordan Hatch <jordan@jordanh.net>
2015-03-01 18:04:40 +0000

    Remove commented out methods from Organisation
    I'm not going to make organisations work properly just yet, but it makes
    sense to at least remove the methods that worked with the Need API.

--- app/models/organisation.rb
+++ app/models/organisation.rb
@@ -1,7 +1,4 @@
 class Organisation
-
-  # attr_reader :id, :name, :abbreviation, :status
-
   include Mongoid::Document
 
   field :name, type: String
@@ -10,22 +7,6 @@ class Organisation
   field :abbreviation, type: String, default: ""
   field :parent_ids, type: Array, default: []
   field :child_ids, type: Array, default: []
-
-
-  # def self.cache
-  #   @cache ||= LRUCache.new(ttl: 1.hour)
-  # end
-
-  # def self.reset_cache
-  #   @cache = nil
-  # end
-  #
-  # def initialize(atts)
-  #   @id = atts[:id]
-  #   @name = atts[:name]
-  #   @abbreviation = atts[:abbreviation]
-  #   @status = atts[:govuk_status]
-  # end
 
   def name_with_abbreviation_and_status
     if abbreviation.present? && abbreviation != name
@@ -37,26 +18,4 @@ class Organisation
       "#{name} (#{status})"
     end
   end
-
-  # def self.all
-  #   cache.fetch('all_organisations') do
-  #     # gds-api-adapters would cache the HTTP response for us if cache headers
-  #     # were set, but it wouldn't be at all obvious to readers of this code and
-  #     # we'd still have parse the JSON and convert into our Organisation models.
-  #     # Even if they were set now, cache headers might get changed in the future
-  #     # and ruin our performance.
-  #     load_organisations
-  #   end
-  # end
-
-  private
-  # def self.load_organisations
-  #   (need_api.organisations || []).map {|atts|
-  #     self.new(atts.symbolize_keys)
-  #   }
-  # end
-
-  # def self.need_api
-  #   Maslow.need_api
-  # end
 end
