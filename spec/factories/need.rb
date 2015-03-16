71a683dc9d9f0ba52d94110003085f1b752e3b7a
Jordan Hatch <jordan@jordanh.net>
2015-03-16 20:40:25 +0000

    Add `met_when` value to the Need factory

--- spec/factories/need.rb
+++ spec/factories/need.rb
@@ -4,6 +4,10 @@ FactoryGirl.define do
     role 'user'
     goal 'do a thing'
     benefit 'get a benefit'
+    met_when [
+      'the first criteria is met',
+      'the second criteria is met',
+    ]
   end
 
 end
