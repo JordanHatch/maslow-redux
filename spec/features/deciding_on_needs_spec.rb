1d2a71c2b9da343d3e214428e0126f2fc626ccb1
Jordan Hatch <jordan@jordanh.net>
2015-03-21 15:26:28 +0000

    Allow needs to be closed from 'decide on this need'
    This adds a feature spec for closing a need, and adds a link to the 
    new "Decide on this need" dropdown menu to allow a need to be closed as
    a duplicate.

--- spec/features/deciding_on_needs_spec.rb
+++ spec/features/deciding_on_needs_spec.rb
@@ -25,4 +25,22 @@ RSpec.describe 'deciding on needs', type: :feature do
     end
   end
 
+  it 'can close a need as a duplicate, and then re-open it' do
+    canonical_need = create(:need)
+
+    visit need_path(need)
+    click_on 'Close as duplicate'
+
+    fill_in 'This need is a duplicate of', with: canonical_need.id
+    click_on 'Close as a duplicate'
+
+    expect(page).to have_content('This need has been closed')
+    expect(page).to have_content(canonical_need.goal)
+    expect(page).to_not have_content('Decide on this need')
+
+    click_on 'Reopen this need'
+
+    expect(page).to_not have_content('This need has been closed')
+  end
+
 end
