db07399cf6b9898cb50aaf58f5cfe6e67bbb6cbd
Jordan Hatch <jordan@jordanh.net>
2019-01-28 23:21:45 +1100

    Allow draggable editing of tag pages

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,8 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require jquery-ui/widgets/draggable
+//= require jquery-ui/widgets/sortable
 //= require chosen-jquery
 //= require autosize
 //= require bootstrap
