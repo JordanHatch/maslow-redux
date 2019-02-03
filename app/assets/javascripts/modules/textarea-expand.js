edfda263c7df7008490945de58cea08e716a0785
Jordan Hatch <jordan@jordanh.net>
2019-02-03 18:33:29 +1100

    Make form hiding optional on textarea-expand module

--- app/assets/javascripts/modules/textarea-expand.js
+++ app/assets/javascripts/modules/textarea-expand.js
@@ -7,18 +7,22 @@ $(function() {
     var $label = $form.find('label')
     var $submit = $form.find("input[type='submit']")
 
+    var hideFormUntilActive = $input.attr('data-expand-hide-form-until-active')
+
     autosize($input)
     $form.addClass('inactive')
 
     $label.hide()
     $input.attr('placeholder', $label.text() + '...')
 
-    $submit.hide()
-    $input.on('focus', function () {
-      $form.removeClass('inactive').addClass('active')
-      $input.removeAttr('placeholder')
-      $submit.show()
-    })
+    if (hideFormUntilActive) {
+      $submit.hide()
+      $input.on('focus', function () {
+        $form.removeClass('inactive').addClass('active')
+        $input.removeAttr('placeholder')
+        $submit.show()
+      })
+    }
   })
 
 })
