370a5d9a4a6f9c66a88d11f7a191fb10f092bf55
Jordan Hatch <jordan@jordanh.net>
2019-02-03 18:28:17 +1100

    Format textarea-expand.js to standard style
    (or as close as can be)

--- app/assets/javascripts/modules/textarea-expand.js
+++ app/assets/javascripts/modules/textarea-expand.js
@@ -1,24 +1,24 @@
-$(function(){
+$(function() {
 
-  $("textarea[data-module='expand']").each(function(){
-    var $input = $(this);
+  $("textarea[data-module='expand']").each(function () {
+    var $input = $(this)
 
-    var $form = $input.parents('form');
-    var $label = $form.find("label");
-    var $submit = $form.find("input[type='submit']");
+    var $form = $input.parents('form')
+    var $label = $form.find('label')
+    var $submit = $form.find("input[type='submit']")
 
-    autosize($input);
-    $form.addClass('inactive');
+    autosize($input)
+    $form.addClass('inactive')
 
-    $label.hide();
-    $input.attr('placeholder', $label.text() + '...');
+    $label.hide()
+    $input.attr('placeholder', $label.text() + '...')
 
-    $submit.hide();
-    $input.on('focus', function(){
-      $form.removeClass('inactive').addClass('active');
-      $input.removeAttr('placeholder');
-      $submit.show();
-    });
-  });
+    $submit.hide()
+    $input.on('focus', function () {
+      $form.removeClass('inactive').addClass('active')
+      $input.removeAttr('placeholder')
+      $submit.show()
+    })
+  })
 
-});
+})
