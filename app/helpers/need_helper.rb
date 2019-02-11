a2a87517925e4bf9e9bd93d25f4096e0996dfbcd
Jordan Hatch <jordan@jordanh.net>
2019-02-11 22:25:46 +1100

    Remove redundant helper methods

--- app/helpers/need_helper.rb
+++ app/helpers/need_helper.rb
@@ -16,31 +16,6 @@ module NeedHelper
     criteria.present? ? criteria : [""]
   end
 
-  def calculate_percentage(numerator, denominator)
-    return unless numerator.present? and denominator.present?
-    return if denominator == 0
-
-    percent = numerator.to_f / denominator.to_f * 100.0
-
-    # don't include the fractional part if the percentage is X.0%
-    format = percent.modulo(1) < 0.1 ? "%.0f%%" : "%.1f%%"
-    (format % percent)
-  end
-
-  def show_interactions_column?(need)
-    [ need.yearly_user_contacts, need.yearly_site_views, need.yearly_need_views, need.yearly_searches ].select(&:present?).any?
-  end
-
-  def format_friendly_integer(number)
-    if number >= 1000000
-      "%.3g\m" % (number.to_f / 1000000)
-    elsif number >= 1000
-      "%.3g\k" % (number.to_f / 1000)
-    else
-      number.to_s
-    end
-  end
-
   def paginate_needs(needs)
     paginate needs
   end
